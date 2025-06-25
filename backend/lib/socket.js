5864ee472ee1af58e7d0da1d52dd0ddf96f63c7c
Arnab <arnavmallick12006@gmail.com>
2025-06-26 02:25:33 +0530

    cors socket

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'], // Adjust this to your frontend URL
+    origin: ['https://ping-me-navy.vercel.app/'], // Adjust this to your frontend URL
   },
 });
 
